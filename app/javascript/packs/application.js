d95101b25d5f2a6d3dcce060504496e9cae78270
Trevor <tlow94@uw.edu>
2021-01-15 14:15:19 -0800

    devise styling bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,20 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+// require("bootstrap")
+// import "../stylesheets/application";
+// document.addEventListener("turbolinks:load", function() {
+//     $(function () {
+//         $('[data-toggle="tooltip"]').tooltip()
+//         $('[data-toggle="popover"]').popover()
+//     })
+// })
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+// require("packs/custom")
+// require("packs/posts.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +24,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "bootstrap"
