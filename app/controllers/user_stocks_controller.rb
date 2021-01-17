23208639d6bd5d02858ee2b365a3c6d61c2cee4f
Trevor <tlow94@uw.edu>
2021-01-17 14:17:17 -0800

    remove tracking

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -11,4 +11,11 @@ class UserStocksController < ApplicationController
         redirect_to my_portfolio_path
     end
 
+    def destroy
+        stock = Stock.find(params[:id])
+        user_stock = UserStock.where(user_id: current_user.id, stock_id: stock).first.destroy
+        flash[:notice] = "#{stock.ticker} was successfully removed from portofolio"
+        redirect_to my_portfolio_path
+    end
+
 end
