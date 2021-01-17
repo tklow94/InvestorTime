21524f16c64e59fd46594e6baa49cb7d4adcfc68
Trevor <tlow94@uw.edu>
2021-01-16 16:28:13 -0800

    many to many userstocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+
+    validates :name, :ticker, presence: true
 
     def self.new_lookup(ticker_symbol) #self allows us to directly call this from the class not an instance of the class
         client = IEX::Api::Client.new(
