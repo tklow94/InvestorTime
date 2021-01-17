3289a96420c869f599b61959e19ad0a4d1365f60
Trevor <tlow94@uw.edu>
2021-01-16 16:48:14 -0800

    stock table views and partials

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,8 @@
 class Stock < ApplicationRecord
     has_many :user_stocks
     has_many :users, through: :user_stocks
+    
+    
 
     validates :name, :ticker, presence: true
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    has_many :stocks, through: :user_stocks
     has_many :user_stocks
+    has_many :stocks, through: :user_stocks
+    
 end
