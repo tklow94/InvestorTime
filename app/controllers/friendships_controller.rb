42b99741ff4d1b01b62b55fbbae5f0922a4e7d7d
Trevor <tlow94@uw.edu>
2021-01-18 13:19:24 -0800

    add friend UI

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,14 @@
 class FriendshipsController < ApplicationController
 
   def create
-
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "Following User"
+    else
+      flash[:alert] = "There was something wrong with the tracking request"
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
